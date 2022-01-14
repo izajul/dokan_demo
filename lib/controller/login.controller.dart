c12bd20068fbb1480cfafa23265bbcbb6b4cfc77
Mind Game <md.izajul@gmail.com>
2022-01-14 17:31:23 +0600

    login.controller.dart created

--- lib/controller/login.controller.dart
+++ lib/controller/login.controller.dart
@@ -1,24 +1,34 @@
 import 'package:dokan/model/login.model.dart';
 import 'package:dokan/services/remote.service.dart';
+import 'package:dokan/utils/functions.dart';
 import 'package:get/get.dart';
 
 class LoginController extends GetxController {
   var isLoading = false.obs;
   var response = LoginModel().obs;
   var responseErr = LoginModelErr().obs;
+  final _payload = {"username": "", "password": ""}.obs;
 
-  void login(Map<String, String> payload) async {
+  Map<String, String> get payload => _payload.value;
+
+  void login() async {
     isLoading(true);
     var res = await RemoteService().login(payload);
     if (res.status) {
       LoginModel data = res.data as LoginModel;
       response.value = data;
-      // todo :: need to save the session into local
+
+      showSuccessSnack("Login Success",
+          "Hello ${data.userDisplayName}, You are successfully logged in ");
     } else {
       LoginModelErr data = res.data as LoginModelErr;
       responseErr.value = data;
-      Get.snackbar("Login Failed", "${data.message}").show();
+      showErrorSnack("Login Failed", data.message ?? "Unable to login");
     }
     isLoading(false);
+  }
+
+  void setData(String key, String data) async {
+    _payload.value[key] = data;
   }
 }
