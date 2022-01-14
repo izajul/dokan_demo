5d1ff97ac1cf674ab6af18bb1aca545fd230704a
Mind Game <md.izajul@gmail.com>
2022-01-14 21:08:14 +0600

    Registration Model added

---
+++ lib/model/signup.model.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+
+SignupModel signupModelFromJson(String str) =>
+    SignupModel.fromJson(json.decode(str));
+
+String signupModelToJson(SignupModel data) => json.encode(data.toJson());
+
+class SignupModel {
+  SignupModel({
+    this.code,
+    this.message,
+    this.data,
+  });
+
+  var code;
+  String? message;
+  Data? data;
+
+  factory SignupModel.fromJson(Map<String, dynamic> json) => SignupModel(
+        code: json["code"],
+        message: json["message"],
+        data: json["data"] == null ? null : Data.fromJson(json["data"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "code": code,
+        "message": message,
+        "data": data?.toJson(),
+      };
+}
+
+class Data {
+  Data({
+    this.status,
+  });
+
+  var status;
+
+  factory Data.fromJson(Map<String, dynamic> json) => Data(
+        status: json["status"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "status": status,
+      };
+}
